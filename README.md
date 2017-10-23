ef88aad5c25dc3dbc8289d81ef68049bf50952cb
walker.zheng <walker.zheng@inforun.net>
2017-10-23 18:05:33 +0800

    update
    Signed-off-by: walker.zheng <walker.zheng@inforun.net>

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+1. 添加 makefile template;
+2. 添加 CPATH/LD_LIBRARY_PATH;
---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+#include "test.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello, test" << std::endl;
+    test_range_zip();
+    return 0;
+}
