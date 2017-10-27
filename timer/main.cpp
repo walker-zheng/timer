de2fc7b4d595756bd67fdea87656424ef3474803
walker.zheng <walker.zheng@inforun.net>
2017-10-27 14:47:25 +0800

    1. 修改 多个timer结合使用；

--- timer/main.cpp
+++ timer/main.cpp
@@ -12,7 +12,7 @@ void foo()
 
 void done()
 {
-    std::cout << (duration<double>(system_clock::now() - now)).count() << "s => Done!" << std::endl;
+    std::cout << "> " << (duration<double>(system_clock::now() - now)).count() << "s => Done!" << std::endl;
 }
 
 struct bar
@@ -23,33 +23,81 @@ struct bar
     }
 };
 
-int test_timer()
+int test_loop_timer()
 {
+    easy::Timer g_timer;
+    g_timer.add([ = ]()
     {
         easy::Timer timer;
         bar b;
         std::vector<int> taskids;
-        std::cout << (duration<double>(system_clock::now() - now)).count() << "s => Start!" << std::endl;
-        taskids.push_back(timer.add(foo, now + seconds(2)));
-        taskids.push_back(timer.add(std::bind(&bar::hello, b), now + seconds(3), seconds(3), 3));
+        auto curr = system_clock::now();
+        std::cout << "> timer " << ": " << (duration<double>(curr - now)).count() << "s => Start!" << std::endl;
+        taskids.push_back(timer.add(foo, curr + seconds(2)));
+        taskids.push_back(timer.add(std::bind(&bar::hello, b), curr + seconds(3), seconds(3), 3));
         taskids.push_back(timer.add(
         [ = ]() { std::cout << (duration<double>(system_clock::now() - now)).count() << "s => func lambda" << std::endl; },
-        now + seconds(3), seconds(1)));
+        curr + seconds(4), seconds(2)));
         std::this_thread::sleep_for(seconds(10));
 
-        for (auto i : taskids)
+        for (auto id : taskids)
         {
-            timer.del(i);
+            std::cout << "> timer " << ": del " << id << std::endl;
+            timer.del(id);
         }
 
-        std::this_thread::sleep_for(seconds(5));
         timer.stop();
+        curr = system_clock::now();
+        std::cout << "> timer " << ": " << (duration<double>(curr - now)).count() << "s => Stop!" << std::endl;
+    },
+    system_clock::now() + seconds(4), seconds(15));
+    std::this_thread::sleep_for(seconds(60));
+    return 0;
+}
+int test_timer()
+{
+    std::vector<std::thread> timers;
+
+    for (auto i = 0; i < 5; i++)
+    {
+        timers.emplace_back([i]()
+        {
+            std::this_thread::sleep_for(seconds(12 * i));
+            easy::Timer timer;
+            bar b;
+            std::vector<int> taskids;
+            auto curr = system_clock::now();
+            std::cout << "> timer " << i << ": " << (duration<double>(curr - now)).count() << "s => Start!" << std::endl;
+            taskids.push_back(timer.add(foo, curr + seconds(2)));
+            taskids.push_back(timer.add(std::bind(&bar::hello, b), curr + seconds(3), seconds(3), 3));
+            taskids.push_back(timer.add(
+            [ = ]() { std::cout << (duration<double>(system_clock::now() - now)).count() << "s => func lambda" << std::endl; },
+            curr + seconds(4), seconds(2)));
+            std::this_thread::sleep_for(seconds(10));
+
+            for (auto id : taskids)
+            {
+                std::cout << "> timer " << i << ": del " << id << std::endl;
+                timer.del(id);
+            }
+
+            timer.stop();
+            curr = system_clock::now();
+            std::cout << "> timer " << i << ": " << (duration<double>(curr - now)).count() << "s => Stop!" << std::endl;
+        });
     }
+
+    for (auto & th : timers)
+    {
+        th.join();
+    }
+
     done();
     return 0;
 }
 int main()
 {
-    test_timer();
+    test_loop_timer();
+    //test_timer();
     return 0;
 }
