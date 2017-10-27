392b44abcbeb37b622641d1e9fd23e66fddd2040
walker.zheng <walker.zheng@inforun.net>
2017-10-27 13:08:43 +0800

    clean project
    Signed-off-by: walker.zheng <walker.zheng@inforun.net>

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,8 @@
 #include "main.h"
-#include "test.h"
 #include "timer.h"
-#include <iostream>
 
 int main()
 {
-    std::cout << "hello, test" << std::endl;
-    //test_range_zip();
     test_timer();
     return 0;
 }
